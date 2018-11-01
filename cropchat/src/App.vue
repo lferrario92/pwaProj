105e7412060a8a8900956d38f8702d56224f71c0
Lucas Ferrario <lucas.ferrario@bigbox.com.ar>
2018-11-01 16:04:51 -0300

    following the tutorial, first dummy data added.

--- cropchat/src/App.vue
+++ cropchat/src/App.vue
@@ -1,22 +1,42 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">CropChat</span>
+      </div>
     </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">CropChat</span>
+      <nav class="mdl-navigation">
+        <router-link class="mdl-navigation__link" to="/" @click.native="hideMenu">Home</router-link>
+        <router-link class="mdl-navigation__link" to="/post" @click.native="hideMenu">Post a picture</router-link>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <router-view></router-view>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
+require('material-design-lite')
 export default {
-  name: 'app'
+  name: 'app',
+  methods: {
+    hideMenu: function () {
+      document.getElementsByClassName('mdl-layout__drawer')[0].classList.remove('is-visible')
+      document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.remove('is-visible')
+    }
+  }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+@import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
+
 body {
   margin: 0;
 }
